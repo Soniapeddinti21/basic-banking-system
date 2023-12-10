f3e73c2061104b51c6a54b8a163ff073a1ef31a4
Soniapeddinti21 <121163741+Soniapeddinti21@users.noreply.github.com>
2023-12-10 02:04:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </div>
       </div>
       <footer class="text-center mt-5 py-2">
-        <p> Made by <b>YAGNESH ARASADA</b>
+        <p> Made by <b>Sonia Peedinti</b>
       </footer>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
